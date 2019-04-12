b2d900c530285c46d1afa127c7621f2ba66bb61f
happyjoo <happy_joo@163.com>
2019-04-12 19:05:32 +0800

    update

--- python3/136_Single_number.py
+++ python3/136_Single_number.py
@@ -2,17 +2,19 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Remember there is only one single number in list.
         # All of the others appear twice.
-        # First solution:
+        # presuppose the nums is an odd list already.
+        # Solution 1:
         result = 0
         for i in range(len(nums)):
             result ^= nums[i]
+        return result
         '''
         # The thing up there (60ms) is two time faster than below (112ms),
         # don't know why yet..
         for i in nums:
             result ^= i
         '''
-        return result
+        
 ''' 
     So the point is, every element appears twice except the one we're finding. 
     Note here, TWICE!! Only ONE single number!
@@ -26,7 +28,7 @@ class Solution:
     And then only the single one is left~
 '''
 
-        # Second solution(iterator):
+        # Solution 2 (iterator):
         res = {} # Get an empty dict
         for i in nums: # Iterate the list
             if i in res:
@@ -38,10 +40,10 @@ class Solution:
                 return key # According to the question, there will be one only answer.
 
 
+        # Solution 3
         nums.sort()
-        for i in range(0,len(nums)-2,2):
-            x = i+1
-            if nums[i] != nums[x]:
-                return nums[i]
+        for i in range(0,len(nums)-1,2):
+            if nums[i] != nums[i + 1]: # Every two numbers
+                return nums[i] # Because there is only one number we're looking for
         return nums[-1]
 
