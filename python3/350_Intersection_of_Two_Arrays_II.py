082b29d86772d619d120a072e68e6fbfa28c4dd6
HappyJoo <Happy_Joo@163.com>
2019-04-14 10:06:29 +0800

    350

---
+++ python3/350_Intersection_of_Two_Arrays_II.py
@@ -0,0 +1,17 @@
+""" Given two arrays, write a function to compute their intersection."""
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        x1, x2 = 0, 0
+        nums1.sort()
+        nums2.sort()
+        while x1 < len(nums1) and x2 < len(nums2):
+            if nums1[x1] == nums2[x2]:
+                res.append(nums1[x1])
+                x1 += 1
+                x2 += 1
+            elif nums1[x1] > nums2[x2]:
+                x2 += 1
+            else:
+                x1 += 1
+        return res
