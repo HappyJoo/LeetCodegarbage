c87d5a3ecad0276047f80451fcdd9a9db982910a
happyjoo <happy_joo@163.com>
2020-04-29 10:38:20 +0800

    update'

--- python3/014_Longest_Common_Prefix.py
+++ python3/014_Longest_Common_Prefix.py
@@ -1,7 +1,7 @@
 # Solution 1 compare every character vertically in **two row**
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        r = [len(set(c)) == 1 for c in zip(*str)] + [0]
+        r = [len(set(c)) == 1 for c in zip(*strs)] + [0]
         return str[0][:r.index(0)] if strs else ''
 
 
@@ -20,15 +20,12 @@ class Solution:
 # Solution 3 compare one by one
 class Solution:
     def longestCommonPrefix(self, s: List[str]) -> str:
-        if not s:
-            return ""
-        res = s[0]
-        i = 1
-        while i < len(s):
-            while s[i].find(res) != 0:
-                res = res[0:len(res)-1]
-            i += 1
-        return res
+        if not strs: return ''
+        a = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(a) != 0:
+                a = a[0:len(a)-1]
+        return a
 
 # Solution 4 compare the first and last one after sorting.
 class Solution:
